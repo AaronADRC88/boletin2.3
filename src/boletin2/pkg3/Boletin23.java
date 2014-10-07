adaed0dc92cbe27f49be9bbf8e5245983981b616
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-10-07 08:58:10 +0200

    €2dolars con interfaz

---
+++ src/boletin2/pkg3/Boletin23.java
@@ -0,0 +1,27 @@
+/*cambio €-$*/
+package boletin2.pkg3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aferreiradominguez
+ */
+public class Boletin23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      float euros ;
+      System.out.println("euros= ");
+      Scanner dato=new Scanner(System.in);
+      euros=dato.nextFloat();
+      float dolares ;
+      System.out.println("dolares= "+euros*1.25);
+      
+      
+      
+    }
+    
+}
